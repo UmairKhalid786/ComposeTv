e9862ec2872265e3e5d3f50d8399e6162657faaa
Umair Khalid <Umair.khalid786@outlook.com>
2023-02-01 01:08:28 +0500

    Exit confirmation dialog added

---
+++ app/src/main/java/com/techlads/composetv/CustomDialogUI.kt
@@ -0,0 +1,128 @@
+package com.techlads.composetv
+
+import android.annotation.SuppressLint
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.*
+import androidx.compose.runtime.*
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.ColorFilter
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.window.Dialog
+import com.techlads.composetv.theme.Gray20
+import com.techlads.composetv.theme.Gray700
+import com.techlads.composetv.theme.LightBlue
+
+@Composable
+fun CustomDialog(openDialogCustom: MutableState<Boolean>, onExitClick: () -> Unit) {
+    Dialog(onDismissRequest = { openDialogCustom.value = false }) {
+        CustomDialogUI(openDialogCustom = openDialogCustom) {
+            onExitClick()
+        }
+    }
+}
+
+//Layout
+@Composable
+fun CustomDialogUI(
+    modifier: Modifier = Modifier,
+    openDialogCustom: MutableState<Boolean>,
+    onExitClick: () -> Unit
+) {
+    Card(
+        shape = RoundedCornerShape(10.dp),
+        modifier = Modifier.padding(10.dp, 5.dp, 10.dp, 10.dp),
+    ) {
+        Column(modifier.background(Color.White)) {
+
+            //.......................................................................
+            Image(
+                painter = painterResource(id = R.drawable.ic_info),
+                contentDescription = null, // decorative
+                contentScale = ContentScale.Fit,
+                colorFilter = ColorFilter.tint(
+                    color = LightBlue
+                ),
+                modifier = Modifier
+                    .padding(top = 35.dp)
+                    .height(70.dp)
+                    .fillMaxWidth(),
+
+                )
+
+            Column(modifier = Modifier.padding(16.dp)) {
+                Text(
+                    text = "Confirmation",
+                    textAlign = TextAlign.Center,
+                    modifier = Modifier
+                        .padding(top = 5.dp)
+                        .fillMaxWidth(),
+                    style = MaterialTheme.typography.labelLarge,
+                    maxLines = 2,
+                    overflow = TextOverflow.Ellipsis
+                )
+                Text(
+                    text = "Do you really want to exit app ?",
+                    textAlign = TextAlign.Center,
+                    modifier = Modifier
+                        .padding(top = 10.dp, start = 25.dp, end = 25.dp)
+                        .fillMaxWidth(),
+                    style = MaterialTheme.typography.bodyMedium
+                )
+            }
+            //.......................................................................
+            Row(
+                Modifier
+                    .fillMaxWidth()
+                    .padding(top = 10.dp)
+                    .background(Gray20.copy(0.2F)),
+            ) {
+                TextButton(onClick = {
+                    openDialogCustom.value = false
+                }, modifier = Modifier.weight(1F)) {
+                    Text(
+                        "No",
+                        fontWeight = FontWeight.Bold,
+                        color = Gray700,
+                        modifier = Modifier.padding(top = 5.dp, bottom = 5.dp)
+                    )
+                }
+
+                Divider(
+                    Modifier
+                        .height(50.dp)
+                        .width(1.dp)
+                )
+
+                TextButton(onClick = {
+                    openDialogCustom.value = false
+                    onExitClick()
+                }, modifier = Modifier.weight(1F)) {
+                    Text(
+                        "Yes",
+                        fontWeight = FontWeight.ExtraBold,
+                        color = Color.Black,
+                        modifier = Modifier.padding(top = 5.dp, bottom = 5.dp)
+                    )
+                }
+            }
+        }
+    }
+}
+
+
+@SuppressLint("UnrememberedMutableState")
+@Preview(name = "Custom Dialog")
+@Composable
+fun MyDialogUIPreview() {
+    CustomDialogUI(openDialogCustom = mutableStateOf(false)) {}
+}
