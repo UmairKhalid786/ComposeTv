8078a219851c19e7f46bea5af44d767273bbb749
Umair <Umair.khalid786@outlook.com>
2023-03-26 22:15:23 +0200

    Home and hero items added

--- app/src/main/java/com/techlads/composetv/home/HomeScreenContent.kt
+++ app/src/main/java/com/techlads/composetv/home/HomeScreenContent.kt
@@ -3,17 +3,34 @@ package com.techlads.composetv.home
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.wrapContentWidth
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.techlads.composetv.home.navigation.NestedHomeNavigation
 import com.techlads.composetv.leftmenu.LeftMenu
+import com.techlads.composetv.leftmenu.data.MenuData
 
 @Composable
 fun HomeScreenContent() {
-   Row {
-       LeftMenu(modifier = Modifier.wrapContentWidth(), defaultFocus = 0, menuItems = MenuData.menuItems) {}
-       NestedHomeNavigation()
-   }
+    val isExpanded = remember {
+        mutableStateOf(false)
+    }
+
+    Row {
+        LeftMenu(
+            modifier = Modifier.wrapContentWidth(),
+            defaultFocus = 0,
+            isExpanded = isExpanded.value,
+            menuItems = MenuData.menuItems,
+            onMenuFocused = { _, isSelected ->
+                isExpanded.value = !isExpanded.value && isSelected
+            }
+        ) {
+
+        }
+        NestedHomeNavigation()
+    }
 }
 
 @Preview
--- app/src/main/java/com/techlads/composetv/home/navigation/NestedHomeNavigation.kt
+++ app/src/main/java/com/techlads/composetv/home/navigation/NestedHomeNavigation.kt
@@ -1,8 +1,74 @@
 package com.techlads.composetv.home.navigation
 
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.focusable
+import androidx.compose.foundation.layout.*
+import androidx.compose.material3.Card
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.tv.foundation.lazy.list.TvLazyColumn
+import androidx.tv.foundation.lazy.list.TvLazyRow
+import com.techlads.composetv.hero.HeroItem
 
 @Composable
-fun NestedHomeNavigation(){
+fun NestedHomeNavigation() {
+    Column(Modifier.fillMaxSize()) {
+        HeroItem()
+        HomeCarousel(Modifier.weight(1f))
+    }
+}
 
+@Composable
+fun HomeCarousel(modifier: Modifier) {
+    TvLazyColumn(modifier) {
+        items(15) {
+            HorizontalCarouselItem(it)
+        }
+    }
+}
+
+@Composable
+fun HorizontalCarouselItem(index: Int) {
+    Column(Modifier.height(150.dp)) {
+        Text(text = "Row $index", modifier = Modifier.padding(horizontal = 24.dp))
+        TvLazyRow(
+            contentPadding = PaddingValues(
+                start = 16.dp,
+                top = 8.dp,
+                bottom = 8.dp,
+                end = 100.dp
+            )
+        ) {
+            items(15) {
+                CarouselItem(index, it)
+            }
+        }
+    }
+}
+
+@Composable
+fun CarouselItem(parent: Int, child: Int) {
+    Card(
+        modifier = Modifier
+            .padding(horizontal = 8.dp)
+            .fillMaxHeight()
+            .aspectRatio(1.8f)
+            .clickable { }
+            .focusable()
+    ) {
+        Box(contentAlignment = Alignment.Center, modifier = Modifier.fillMaxSize()) {
+            Text(text = "Item $parent x $child", textAlign = TextAlign.Center)
+        }
+    }
+}
+
+@Preview
+@Composable
+fun NestedHomeNavigationPrev() {
+    NestedHomeNavigation()
 }
