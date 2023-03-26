f944739ddfd17c55ca24995841fb3867ef708983
Umair <Umair.khalid786@outlook.com>
2023-03-26 01:22:12 +0100

    Left menu basic structure added

--- app/src/main/java/com/techlads/composetv/home/HomeScreenContent.kt
+++ app/src/main/java/com/techlads/composetv/home/HomeScreenContent.kt
@@ -1,14 +1,22 @@
 package com.techlads.composetv.home
 
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.width
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.techlads.composetv.home.navigation.NestedHomeNavigation
 import com.techlads.composetv.leftmenu.LeftMenu
 
 @Composable
 fun HomeScreenContent() {
-    LeftMenu(defaultFocus = 2,menuItems = MenuData.menuItems) {
+   Row {
+       LeftMenu(modifier = Modifier.width(200.dp), defaultFocus = 0, menuItems = MenuData.menuItems) {
 
-    }
+       }
+       NestedHomeNavigation()
+   }
 }
 
 @Preview
---
+++ app/src/main/java/com/techlads/composetv/home/navigation/NestedHomeNavigation.kt
@@ -0,0 +1,8 @@
+package com.techlads.composetv.home.navigation
+
+import androidx.compose.runtime.Composable
+
+@Composable
+fun NestedHomeNavigation(){
+
+}
