505f07f5d5cc8dc4cf4b1a9dd971f6b4e990e07e
Umair <Umair.khalid786@outlook.com>
2023-04-12 02:14:07 +0200

    Settings sub navigation split into smaller composable

--- app/src/main/java/com/techlads/composetv/settings/SettingsScreen.kt
+++ app/src/main/java/com/techlads/composetv/settings/SettingsScreen.kt
@@ -4,16 +4,11 @@ import androidx.compose.animation.ExperimentalAnimationApi
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavHostController
-import androidx.tv.foundation.lazy.list.TvLazyColumn
-import androidx.tv.material3.Text
 import com.google.accompanist.navigation.animation.rememberAnimatedNavController
-import com.techlads.composetv.settings.data.SettingsMenuModel
-import com.techlads.composetv.widgets.FocusableItem
 import com.techlads.composetv.settings.navigation.NestedSettingsScreenNavigation
 import com.techlads.composetv.theme.AppTheme
 
@@ -40,26 +35,6 @@ fun SettingsScreen() {
 @Composable
 fun SettingsNavigation(navController: NavHostController) {
     NestedSettingsScreenNavigation(navController = navController)
-}
-
-@Composable
-fun SettingsMenu(modifier: Modifier, onMenuSelected: (SettingsMenuModel) -> Unit) {
-    val settingsMenu = remember {
-        SettingsMenuData.menu
-    }
-    TvLazyColumn(modifier = modifier.width(200.dp)) {
-        items(settingsMenu.size) {
-            val item = settingsMenu[it]
-            FocusableItem(
-                modifier = Modifier.padding(horizontal = 16.dp, vertical = 8.dp),
-                onClick = { onMenuSelected(item) }) {
-                Text(
-                    text = item.text,
-                    modifier = Modifier.padding(horizontal = 12.dp, vertical = 8.dp)
-                )
-            }
-        }
-    }
 }
 
 
