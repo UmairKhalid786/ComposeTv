ed02e6908301348879a5808088c9ae7e380dbbcb
Umair <Umair.khalid786@outlook.com>
2023-04-13 02:13:58 +0200

    Carousel Item updated to TV Material 3 APIs

--- app/src/main/java/com/techlads/composetv/home/carousel/CarouselItem.kt
+++ app/src/main/java/com/techlads/composetv/home/carousel/CarouselItem.kt
@@ -23,47 +23,22 @@ import androidx.compose.ui.zIndex
 import androidx.tv.material3.ExperimentalTvMaterial3Api
 import androidx.tv.material3.contentColorFor
 import com.techlads.composetv.theme.AppTheme
+import com.techlads.composetv.widgets.BorderedFocusableItem
 
 
 @OptIn(ExperimentalTvMaterial3Api::class)
 @Composable
 fun CarouselItem(parent: Int, child: Int, onItemFocus: (item: Int) -> Unit) {
-
-    var isFocused by remember { mutableStateOf(false) }
-    val scale = animateFloatAsState(if (isFocused) 1.2f else 1f)
-
-    Card(
+    BorderedFocusableItem(
+        onClick = {  },
         modifier = Modifier
-            .zIndex(if (isFocused) 20f else 1f)
-            .graphicsLayer(
-                scaleX = scale.value,
-                scaleY = scale.value
-            )
             .padding(horizontal = 8.dp)
-            .fillMaxHeight()
             .aspectRatio(1.8f)
             .onFocusChanged {
-                isFocused = it.isFocused
-                if (isFocused) {
+                if (it.isFocused) {
                     onItemFocus(child)
                 }
-            }
-            .border(
-                border = BorderStroke(
-                    1.dp,
-                    if (isFocused)
-                        contentColorFor(backgroundColor = AppTheme.surface)
-                    else
-                        Color.Transparent
-                ), shape = MaterialTheme.shapes.medium
-            )
-            .clickable { }
-            .focusable(),
-        colors = CardDefaults.cardColors(
-            containerColor = AppTheme.surface,
-            contentColor = contentColorFor(backgroundColor = AppTheme.surface)
-        )
-    ) {
+            }) {
         Box(contentAlignment = Alignment.Center, modifier = Modifier.fillMaxSize()) {
             Text(text = "Item $parent x $child", textAlign = TextAlign.Center)
         }
