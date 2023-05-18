7b46addb93560ac38d47c8e74d77471f2cde374d
Yasir Ghafar <yasir896@gmail.com>
2023-05-19 00:25:49 +0500

    Changes required after Review are implemented.

--- app/src/main/java/com/techlads/composetv/details/ProductDetailsScreen.kt
+++ app/src/main/java/com/techlads/composetv/details/ProductDetailsScreen.kt
@@ -3,22 +3,25 @@ package com.techlads.composetv.details
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material3.Button
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.zIndex
+import androidx.tv.material3.LocalContentColor
 import androidx.tv.material3.MaterialTheme
 import androidx.tv.material3.Text
 import com.techlads.composetv.R
+import com.techlads.composetv.theme.AppTheme
+import com.techlads.composetv.theme.Material3Theme
 import com.techlads.composetv.widgets.BorderedFocusableItem
+import com.techlads.composetv.widgets.Button
 
 @Composable
 fun ProductDetailsScreen() {
@@ -28,11 +31,12 @@ fun ProductDetailsScreen() {
 @Composable
 fun ProductDetailsContent() {
     Box {
-        SearchIcon(modifier = Modifier
-            .size(80.dp)
-            .align(Alignment.TopStart)
-            .padding(24.dp)
-            .zIndex(1f)
+        SearchIcon(
+            modifier = Modifier
+                .size(80.dp)
+                .align(Alignment.TopStart)
+                .padding(24.dp)
+                .zIndex(1f)
         )
 
         Column(
@@ -40,9 +44,10 @@ fun ProductDetailsContent() {
                 .align(Alignment.BottomCenter)
                 .fillMaxSize()
         ) {
-            BannerImage(modifier = Modifier
-                .fillMaxSize()
-                .weight(.4f)
+            BannerImage(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .weight(.4f)
             )
             Column(modifier = Modifier.weight(.6f)) {
                 ButtonSection()
@@ -50,10 +55,12 @@ fun ProductDetailsContent() {
             }
         }
 
-        ThumbnailImageCard(modifier = Modifier
-            .align(Alignment.CenterStart)
-            .padding(start = 30.dp),
-            parent = 1, child = 1)
+        ThumbnailImageCard(
+            modifier = Modifier
+                .align(Alignment.CenterStart)
+                .padding(start = 30.dp),
+            parent = 1, child = 1
+        )
     }
 }
 
@@ -82,35 +89,37 @@ fun BannerImage(modifier: Modifier) {
 
 @Composable
 fun ButtonSection() {
-    Row(modifier = Modifier
-        .fillMaxWidth()
-        .background(color = Color(0xFF2D2D44)),
+    Row(
+        modifier = Modifier
+            .fillMaxWidth()
+            .height(60.dp)
+            //.background(color = Color(0xFF2D2D44)),
+            .background(color = AppTheme.surface.copy(alpha = 0.5f)),
         verticalAlignment = Alignment.CenterVertically,
         horizontalArrangement = Arrangement.Start
     ) {
         Spacer(modifier = Modifier.width(280.dp))
+
         Button(
-            modifier = Modifier,
-            shape = RoundedCornerShape(4.dp),
-            contentPadding = PaddingValues(vertical = 0.dp, horizontal = 16.dp),
-            onClick = {  }) {
-            Text(modifier = Modifier.padding(horizontal = 16.dp),
-                text = "Play")
+            text = "Play",
+            modifier = Modifier.padding(horizontal = 16.dp, vertical = 4.dp)
+        ) {
         }
         Spacer(modifier = Modifier.size(16.dp))
         Text(
-            color = Color.White,
+            color = LocalContentColor.current,
             textAlign = TextAlign.Center,
             style = MaterialTheme.typography.titleMedium,
-            text = "Watch Trailer")
+            text = stringResource(R.string.watch_trailer)
+        )
     }
 }
 
 @Composable
 fun DetailsSection() {
-    Row(modifier = Modifier
-        .fillMaxSize()
-        .background(color = Color.Black),
+    Row(
+        modifier = Modifier
+            .fillMaxSize(),
         verticalAlignment = Alignment.Top,
         horizontalArrangement = Arrangement.Center
     ) {
@@ -120,40 +129,54 @@ fun DetailsSection() {
                 .fillMaxWidth()
                 //.weight(.7f)
                 .padding(24.dp),
-            horizontalAlignment = Alignment.Start) {
+            horizontalAlignment = Alignment.Start
+        ) {
             Spacer(modifier = Modifier.height(16.dp))
-            Text(modifier = Modifier,
-                color = Color.White,
+            Text(
+                modifier = Modifier,
+                color = LocalContentColor.current,
                 textAlign = TextAlign.Start,
                 style = MaterialTheme.typography.headlineLarge,
-                text = "Batman")
+                text = stringResource(R.string.movie_name)
+            )
+
             Spacer(modifier = Modifier.size(10.dp))
+
             MovieInfoSection()
+
             Spacer(modifier = Modifier.size(10.dp))
-            Text(modifier = Modifier,
-                color = Color.White,
+            Text(
+                modifier = Modifier,
+                color = LocalContentColor.current,
                 textAlign = TextAlign.Start,
                 style = MaterialTheme.typography.bodyLarge,
-                text = "Batman ventures into Gotham City's underworld when a sadistic killer leaves behind a trail of cryptic clues. ")
+                text = stringResource(R.string.movie_desciption)
+            )
         }
     }
 }
 
 @Composable
 fun MovieInfoSection() {
-    Row(verticalAlignment = Alignment.CenterVertically,
-    horizontalArrangement = Arrangement.Center) {
-        Text(modifier = Modifier,
-            color = Color.White,
+    Row(
+        verticalAlignment = Alignment.CenterVertically,
+        horizontalArrangement = Arrangement.Center
+    ) {
+        Text(
+            modifier = Modifier,
+            color = LocalContentColor.current,
             textAlign = TextAlign.Start,
             style = MaterialTheme.typography.bodyLarge,
-            text = "2022")
+            text = stringResource(R.string.release_year)
+        )
         Spacer(modifier = Modifier.size(12.dp))
-        Text(modifier = Modifier,
-            color = Color.White,
+        Text(
+            modifier = Modifier,
+            color = LocalContentColor.current,
             textAlign = TextAlign.Start,
             style = MaterialTheme.typography.bodyLarge,
-            text = "2h 56mm")
+            text = stringResource(R.string.movie_duration_text)
+        )
         Spacer(modifier = Modifier.size(12.dp))
         Rating(rating = "8.3")
     }
@@ -161,26 +184,30 @@ fun MovieInfoSection() {
 
 @Composable
 fun Rating(rating: String) {
-    Row(verticalAlignment = Alignment.CenterVertically,
-        horizontalArrangement = Arrangement.Center) {
+    Row(
+        verticalAlignment = Alignment.CenterVertically,
+        horizontalArrangement = Arrangement.Center
+    ) {
         Image(
             modifier = Modifier.size(18.dp),
             painter = painterResource(id = R.drawable.ic_star),
             contentDescription = "search",
         )
         Spacer(modifier = Modifier.size(8.dp))
-        Text(modifier = Modifier,
-            color = Color.White,
+        Text(
+            modifier = Modifier,
+            color = LocalContentColor.current,
             textAlign = TextAlign.Start,
             style = MaterialTheme.typography.bodyLarge,
-            text = rating)
+            text = rating
+        )
     }
 }
 
 @Composable
 fun ThumbnailImageCard(modifier: Modifier, parent: Int, child: Int) {
     BorderedFocusableItem(
-        onClick = {  },
+        onClick = { },
         modifier = modifier
             .padding(8.dp)
             .size(width = 150.dp, height = 200.dp)
@@ -193,6 +220,9 @@ fun ThumbnailImageCard(modifier: Modifier, parent: Int, child: Int) {
 
 @Preview
 @Composable
-fun PreviewDetailsScreen() {
-    ProductDetailsScreen()
+fun DetailsScreenPrev() {
+    Material3Theme {
+        ProductDetailsScreen()
+    }
+
 }
