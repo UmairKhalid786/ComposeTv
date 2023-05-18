1295e4bed793e0151501de23524bff12f486ed68
Umair <Umair.khalid786@outlook.com>
2023-05-18 19:32:57 +0200

    Timber log and player listener added

--- app/src/main/java/com/techlads/composetv/MainApp.kt
+++ app/src/main/java/com/techlads/composetv/MainApp.kt
@@ -2,6 +2,14 @@ package com.techlads.composetv
 
 import android.app.Application
 import dagger.hilt.android.HiltAndroidApp
+import timber.log.Timber
 
 @HiltAndroidApp
-class MainApp: Application()
+class MainApp : Application() {
+    override fun onCreate() {
+        super.onCreate()
+        if (BuildConfig.DEBUG) {
+            Timber.plant(Timber.DebugTree())
+        }
+    }
+}
--- app/src/main/java/com/techlads/composetv/features/player/PlayerScreen.kt
+++ app/src/main/java/com/techlads/composetv/features/player/PlayerScreen.kt
@@ -10,10 +10,7 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.DisposableEffect
 import androidx.compose.runtime.LaunchedEffect
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
@@ -23,7 +20,9 @@ import androidx.media3.common.util.UnstableApi
 import androidx.media3.ui.AspectRatioFrameLayout
 import androidx.media3.ui.PlayerView
 import com.techlads.player.PlayerFactory
-import kotlinx.coroutines.delay
+import com.techlads.player.domain.state.PlayerState
+import com.techlads.player.domain.state.PlayerStateListener
+import timber.log.Timber
 
 
 @Composable
@@ -39,16 +38,20 @@ fun PlayerScreenContent(modifier: Modifier, mediaUrl: String, onBackPressed: () 
     val player = remember {
         PlayerFactory.create(context)
     }
-    var contentCurrentPosition: Long by remember { mutableStateOf(0L) }
+
+    val stateListener = remember {
+        object : PlayerStateListener {
+            override fun on(state: PlayerState) {
+                Timber.d("State $state")
+            }
+        }
+    }
 
     BackHandler(onBack = onBackPressed)
 
     LaunchedEffect(Unit) {
         player.prepare(mediaUrl, true)
-        while (true) {
-            delay(300)
-            contentCurrentPosition = 0
-        }
+        player.setPlaybackEvent(callback = stateListener)
     }
 
     Box(modifier = modifier.fillMaxSize(), contentAlignment = Alignment.Center) {
