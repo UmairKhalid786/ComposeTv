64c0b3f1a5348cb85e7112feb8f6b1a04caee285
Umair <Umair.khalid786@outlook.com>
2023-05-20 12:13:24 +0200

    State listener renamed

--- player/src/main/java/com/techlads/player/exoplayer/ExoPlayerStateListener.kt
+++ player/src/main/java/com/techlads/player/exoplayer/ExoPlayerStateListener.kt
@@ -3,39 +3,41 @@ package com.techlads.player.exoplayer
 import androidx.media3.common.Player
 import androidx.media3.common.Player.STATE_BUFFERING
 import androidx.media3.common.Player.STATE_ENDED
-import androidx.media3.common.Player.STATE_IDLE
 import androidx.media3.common.Player.STATE_READY
 import androidx.media3.exoplayer.ExoPlayer
 import com.techlads.player.domain.state.PlayerState
 import com.techlads.player.domain.state.PlayerStateListener
+import timber.log.Timber
 
-internal class ExoPlayerStateListenerBridge(
+internal class ExoPlayerStateListener(
     private val stateListener: PlayerStateListener,
     val player: ExoPlayer
 ) : Player.Listener {
 
+    override fun onIsPlayingChanged(isPlaying: Boolean) {
+        super.onIsPlayingChanged(isPlaying)
+        stateListener.on(getStateWhen(isPlaying))
+    }
+
     override fun onPlaybackStateChanged(playbackState: Int) {
         super.onPlaybackStateChanged(playbackState)
-
         val state = when (playbackState) {
-            STATE_IDLE -> {
-                PlayerState.Idle
-            }
-
-            STATE_BUFFERING -> {
-                PlayerState.Buffering
-            }
-
+            STATE_BUFFERING -> PlayerState.Buffering
             STATE_READY -> {
-                PlayerState.Idle
+                val isPlaying = player.playWhenReady
+                getStateWhen(isPlaying)
             }
 
-            STATE_ENDED -> {
-                PlayerState.Complete
-            }
-
+            STATE_ENDED -> PlayerState.Complete
             else -> PlayerState.Idle
         }
+
+        Timber.d("PlaybackState $state")
         stateListener.on(state)
     }
+
+    private fun getStateWhen(playing: Boolean) = if (playing)
+        PlayerState.Playing
+    else
+        PlayerState.Pause
 }
