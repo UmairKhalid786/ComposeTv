07bc5a41345e31653f214656d6eac677c311ff8f
Umair Khalid <umair.khalid786@outlook.com>
2023-06-15 00:06:43 +0200

    TV material migration (#61)
    * Product detail animation added for portrait size
    
    * Basic player integrated from detail screen
    
    * Controls preview added
    
    * Video title and Description added
    
    * Controls animation updated expand
    Title and description placed at top of player
    
    * Login button migrated to tv material api

--- app/src/main/java/com/techlads/composetv/features/login/withEmailPassword/LoginButton.kt
+++ app/src/main/java/com/techlads/composetv/features/login/withEmailPassword/LoginButton.kt
@@ -1,62 +1,43 @@
+@file:OptIn(ExperimentalTvMaterial3Api::class)
+
 package com.techlads.composetv.features.login.withEmailPassword
 
-import androidx.compose.foundation.focusable
 import androidx.compose.foundation.layout.*
-import androidx.compose.material3.Button
-import androidx.compose.material3.ButtonDefaults
 import androidx.tv.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.focus.onFocusChanged
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import com.techlads.composetv.theme.AppTheme
-import com.techlads.composetv.theme.LightBlue
+import androidx.tv.material3.Button
+import androidx.tv.material3.ExperimentalTvMaterial3Api
 
 @Composable
-fun LoginButton(onClick: () -> Unit) {
-
-    val defaultColor = remember {
-        Color.White.copy(0.6F)
-    }
-    val selectedColor = remember {
-        LightBlue
-    }
-    val selectionColor = remember { mutableStateOf(defaultColor) }
-
-    Box(
-        modifier = Modifier
-            .width(300.dp)
-            .padding(40.dp, 0.dp, 40.dp, 0.dp)
+fun LoginButton(modifier: Modifier = Modifier, onClick: () -> Unit) {
+    Button(
+        onClick = onClick,
+        contentPadding = PaddingValues(16.dp),
+        modifier = modifier
     ) {
-        Button(
-            onClick = onClick,
-            shape = AppTheme.midShape,
-            colors = ButtonDefaults.buttonColors(selectionColor.value),
-            modifier = Modifier
-                .fillMaxWidth()
-                .height(50.dp)
-                .onFocusChanged { state ->
-                    selectionColor.value = if (state.isFocused) {
-                        selectedColor
-                    } else {
-                        defaultColor
-                    }
-                }
-                .focusable(true)
-        ) {
-            Text(
-                text = "LOGIN", style = TextStyle(
-                    color = Color.Black,
-                    fontFamily = FontFamily.SansSerif,
-                    fontWeight = FontWeight.Light
-                )
+        Text(
+            modifier = Modifier.fillMaxWidth(),
+            text = "LOGIN", style = TextStyle(
+                fontFamily = FontFamily.SansSerif,
+                fontWeight = FontWeight.Light,
+                textAlign = TextAlign.Center,
             )
-        }
+        )
+    }
+}
+
+
+@Preview
+@Composable
+fun LoginButtonPrev() {
+    LoginButton(Modifier.wrapContentSize()) {
+
     }
 }
