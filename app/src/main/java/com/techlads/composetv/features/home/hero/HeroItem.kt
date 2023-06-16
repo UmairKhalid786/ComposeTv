a5fc3048597c5e7a83e2d50bdde2d382882e70d8
Umair <Umair.khalid786@outlook.com>
2023-06-16 12:21:41 +0200

    Unused imports removed

--- app/src/main/java/com/techlads/composetv/features/home/hero/HeroItem.kt
+++ app/src/main/java/com/techlads/composetv/features/home/hero/HeroItem.kt
@@ -10,7 +10,6 @@ import androidx.compose.material3.CardDefaults
 import androidx.tv.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
