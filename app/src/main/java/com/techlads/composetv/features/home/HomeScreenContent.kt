2013a7fc021ef286c8e9c47acf12ea8181922a0f
Umair <Umair.khalid786@outlook.com>
2023-06-25 23:58:43 +0200

    Basic menu re work added, replaced with ModalNavigationDrawer

--- app/src/main/java/com/techlads/composetv/features/home/HomeScreenContent.kt
+++ app/src/main/java/com/techlads/composetv/features/home/HomeScreenContent.kt
@@ -2,14 +2,12 @@ package com.techlads.composetv.features.home
 
 import androidx.compose.animation.ExperimentalAnimationApi
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.wrapContentWidth
 import androidx.compose.runtime.*
-import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.hilt.navigation.compose.hiltViewModel
 import com.google.accompanist.navigation.animation.rememberAnimatedNavController
 import com.techlads.composetv.features.home.navigation.NestedHomeNavigation
-import com.techlads.composetv.features.home.leftmenu.LeftMenu
+import com.techlads.composetv.features.home.navigation.drawer.HomeDrawer
 import com.techlads.composetv.theme.ComposeTvTheme
 
 @OptIn(ExperimentalAnimationApi::class)
@@ -18,37 +16,14 @@ fun HomeScreenContent(
     viewModel: HomeViewModel,
     onItemFocus: (parent: Int, child: Int) -> Unit
 ) {
-
-    val menuItems by viewModel.menuItems.collectAsState()
     val navController = rememberAnimatedNavController()
 
-    val isExpanded = remember {
-        mutableStateOf(false)
-    }
-
     Row {
-        LeftMenu(
-            modifier = Modifier.wrapContentWidth(),
-            defaultFocus = 0,
-            isExpanded = isExpanded.value,
-            menuItems = menuItems,
-            onMenuFocused = { _, isSelected ->
-                isExpanded.value = !isExpanded.value && isSelected
-                setViewModelState(isSelected, viewModel)
-            }
-        ) {
+        HomeDrawer(content = {
+            NestedHomeNavigation(navController, onItemFocus)
+        }){
             navController.navigate(it.id)
         }
-
-        NestedHomeNavigation(navController, onItemFocus)
-    }
-}
-
-fun setViewModelState(isSelected: Boolean, viewModel: HomeViewModel) {
-    if (isSelected) {
-        viewModel.menuOpen()
-    } else {
-        viewModel.menuClosed()
     }
 }
 
---
+++ app/src/main/java/com/techlads/composetv/features/home/navigation/drawer/HomeDrawer.kt
@@ -0,0 +1,118 @@
+@file:OptIn(ExperimentalTvMaterial3Api::class)
+
+package com.techlads.composetv.features.home.navigation.drawer
+
+import androidx.compose.runtime.Composable
+import androidx.tv.material3.ExperimentalTvMaterial3Api
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.animation.core.animateDpAsState
+import androidx.compose.animation.core.keyframes
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.tv.material3.DrawerValue
+import androidx.tv.material3.Icon
+import androidx.tv.material3.MaterialTheme
+import androidx.tv.material3.ModalNavigationDrawer
+import androidx.tv.material3.Surface
+import androidx.tv.material3.Text
+import com.techlads.composetv.features.home.leftmenu.MenuHeader
+import com.techlads.composetv.features.home.leftmenu.data.MenuData
+import com.techlads.composetv.features.home.leftmenu.model.MenuItem
+
+val navigationRow: @Composable (
+    drawerValue: DrawerValue,
+    menu: MenuItem,
+    modifier: Modifier,
+    onMenuSelected: ((menuItem: MenuItem) -> Unit)?
+) -> Unit =
+    { drawerValue, menu, modifier, onMenuSelected ->
+        val padding = animateDpAsState(
+            animationSpec = keyframes {
+                this.delayMillis = 100
+            },
+            targetValue = if (drawerValue == DrawerValue.Open) 4.dp else 0.dp,
+        )
+
+        Surface(
+            onClick = { onMenuSelected?.invoke(menu) },
+            modifier =
+            modifier
+                .padding(vertical = 4.dp)
+                .then(if (drawerValue == DrawerValue.Open) modifier.width(170.dp) else modifier)
+        ) {
+            Row(
+                Modifier
+                    .padding(horizontal = 12.dp, vertical = 8.dp),
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                menu.icon?.let {
+                    Icon(
+                        imageVector = it,
+                        contentDescription = menu.text,
+                        modifier = Modifier.size(16.dp)
+                    )
+                    Spacer(modifier = Modifier.padding(horizontal = padding.value))
+                }
+                AnimatedVisibility(
+                    visible = drawerValue == DrawerValue.Open,
+                    modifier = Modifier.height(20.dp)
+                ) {
+                    Text(
+                        text = menu.text,
+                        maxLines = 1,
+                        style = MaterialTheme.typography.bodyMedium
+                    )
+                }
+            }
+        }
+    }
+
+
+@Composable
+fun HomeDrawer(content: @Composable () -> Unit, onMenuSelected: ((menuItem: MenuItem) -> Unit)?) {
+    ModalNavigationDrawer(
+        drawerContent = { drawer ->
+            Column(
+                Modifier
+                    .background(MaterialTheme.colorScheme.surface)
+                    .fillMaxHeight()
+                    .padding(horizontal = 12.dp, vertical = 12.dp),
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                Spacer(modifier = Modifier.height(8.dp))
+                MenuHeader(expanded = drawer == DrawerValue.Open)
+                Spacer(modifier = Modifier.height(16.dp))
+                MenuData.menuItems.forEach {
+                    navigationRow(drawer, it, Modifier) {
+                        onMenuSelected?.invoke(it)
+                    }
+                }
+                Spacer(modifier = Modifier.weight(1f))
+                navigationRow(drawer, MenuData.settingsItem, Modifier) {
+                    onMenuSelected?.invoke(it)
+                }
+            }
+        }, content = content
+    )
+}
+
+
+@Preview
+@Composable
+fun HomeDrawerPrev() {
+    HomeDrawer(content = {
+        Text(text = "Hello World")
+    }, onMenuSelected = null)
+
+}
