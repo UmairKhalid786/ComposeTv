4df4342d967ea928c0ecbc5adb19e653d32c95cc
Umair <Umair.khalid786@outlook.com>
2023-07-02 17:21:07 +0200

    Proper player constructor introduced

--- exoplayer/src/main/java/com/techlads/exoplayer/PlayerFactory.kt
+++ exoplayer/src/main/java/com/techlads/exoplayer/PlayerFactory.kt
@@ -1,16 +1,31 @@
 package com.techlads.exoplayer
 
 import android.content.Context
+import android.view.ViewGroup.LayoutParams.MATCH_PARENT
+import android.widget.FrameLayout
 import androidx.media3.exoplayer.ExoPlayer
+import androidx.media3.ui.AspectRatioFrameLayout
 import androidx.media3.ui.PlayerView
 import com.techlads.player.domain.TLPlayer
 import java.lang.ref.WeakReference
 
 object PlayerFactory {
+
     fun create(
-        context: Context,
-    ): TLPlayer = ExoPlayerImpl(
-        WeakReference(context),
-        ExoPlayer.Builder(context).build()
-    ) { PlayerView(context) }
+        context: Context
+    ): TLPlayer {
+        val exoPlayer = ExoPlayer.Builder(context).build()
+        return ExoPlayerImpl(
+            WeakReference(context),
+            exoPlayer
+        ) {
+            PlayerView(context).apply {
+                hideController()
+                player = exoPlayer
+                useController = false
+                resizeMode = AspectRatioFrameLayout.RESIZE_MODE_FIT
+                layoutParams = FrameLayout.LayoutParams(MATCH_PARENT, MATCH_PARENT)
+            }
+        }
+    }
 }
--- exoplayer/src/main/java/com/techlads/exoplayer/extensions.kt
+++
@@ -1,2 +0,0 @@
-package com.techlads.exoplayer
-
