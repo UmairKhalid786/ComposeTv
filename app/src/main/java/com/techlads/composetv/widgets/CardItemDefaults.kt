043f104d2c36c71954a0c01c735d780a52e9601a
Umair <Umair.khalid786@outlook.com>
2024-06-24 17:36:56 +0500

    Pulse animation for selected item added

---
+++ app/src/main/java/com/techlads/composetv/widgets/CardItemDefaults.kt
@@ -0,0 +1,29 @@
+package com.techlads.composetv.widgets
+
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.ReadOnlyComposable
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.dp
+import androidx.tv.material3.Border
+import androidx.tv.material3.ClickableSurfaceDefaults
+
+object CardItemDefaults {
+    @ReadOnlyComposable
+    @Composable
+    fun border(borderRadius: Dp, color: Color) = ClickableSurfaceDefaults.border(
+        focusedBorder = Border(
+            BorderStroke(
+                width = 2.dp, color = color
+            ), shape = RoundedCornerShape(borderRadius)
+        )
+    )
+
+    @ReadOnlyComposable
+    @Composable
+    fun shape(borderRadius: Dp) = ClickableSurfaceDefaults.shape(
+        shape = RoundedCornerShape(borderRadius), focusedShape = RoundedCornerShape(borderRadius)
+    )
+}
