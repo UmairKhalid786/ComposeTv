d08af144d93bb54cf6607690e14944eb890a646f
Umair Khalid <umair.khalid786@outlook.com>
2025-02-07 09:55:09 +0100

    improve home viewmodel (#137)

--- app/src/main/java/com/techlads/composetv/features/home/HomeViewModel.kt
+++ app/src/main/java/com/techlads/composetv/features/home/HomeViewModel.kt
@@ -20,8 +20,21 @@ class HomeViewModel @Inject constructor(
     private val api: TmdbApiService
 ) : ViewModel() {
 
+    val menuState: StateFlow<Boolean> = MutableStateFlow(false)
+
+    private val _menuItems = MutableStateFlow<List<MenuItem>>(emptyList())
+    val menuItems: StateFlow<List<MenuItem>> = _menuItems.asStateFlow()
+
+    private val _usedTopBar = MutableStateFlow<NavigationEvent>(NavigationEvent.TopBar)
+    val usedTopBar: StateFlow<NavigationEvent> = _usedTopBar.asStateFlow()
+
     private val _movies = MutableStateFlow<List<Movie>>(emptyList())
     val movies: StateFlow<List<Movie>> get() = _movies.asStateFlow()
+
+    init {
+        _menuItems.value = MenuData.menuItems
+        fetchPopularMovies()
+    }
 
     private fun fetchPopularMovies() {
         viewModelScope.launch {
@@ -36,16 +49,6 @@ class HomeViewModel @Inject constructor(
                 // Handle error
             }
         }
-    }
-
-    val menuItems: StateFlow<List<MenuItem>> = MutableStateFlow(emptyList())
-    val menuState: StateFlow<Boolean> = MutableStateFlow(false)
-    private val _usedTopBar: MutableStateFlow<NavigationEvent> = MutableStateFlow(NavigationEvent.TopBar)
-    val usedTopBar: StateFlow<NavigationEvent> = _usedTopBar
-
-    init {
-        menuItems.toMutable().value = MenuData.menuItems
-        fetchPopularMovies()
     }
 
     fun menuClosed() {
